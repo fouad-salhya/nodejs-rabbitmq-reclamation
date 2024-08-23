97a094e08a3b0a9d6321ff054401885243353f5b
fouad-salhya <fouad.salhya@uit.ac.ma>
2024-08-23 07:00:06 +0200

    first commit

--- src/services/RabbitMqService.ts
+++ src/services/RabbitMqService.ts
@@ -7,7 +7,8 @@ class RabbitMqService {
   private channel!: Channel;
   private readonly queue = 'userQueue'
   private readonly reclamationQueue = 'reclQueue'; 
-  private latestReclamationId: string | null = null;
+
+  private lastReclamationId: string | null = null;
 
   async connect() {
     this.connection = await amqp.connect('amqp://localhost');
@@ -22,25 +23,25 @@ class RabbitMqService {
   }
 
 
-  async receiveReclamationId() {
-    this.channel.consume(this.reclamationQueue, (msg) => {
-      if (msg) {
-        const reclamationId = msg.content.toString();
-        this.latestReclamationId = reclamationId; // Stocker le dernier ID reçu
-        console.log(`Received Reclamation ID: ${this.latestReclamationId}`);
-        this.channel.ack(msg); // Confirmer le traitement du message
-      }
-    }, {
-      noAck: false  // Assurez-vous que les messages ne sont pas automatiquement confirmés
+  // Recevoir et mettre à jour la dernière valeur de reclamationId
+  async receiveReclamationId(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.channel.consume(this.reclamationQueue, (msg) => {
+        if (msg) {
+          this.lastReclamationId = msg.content.toString();
+          this.channel.ack(msg);
+          resolve(this.lastReclamationId);
+        } else {
+          reject(new Error('No message received'));
+        }
+      });
     });
   }
 
-  // Nouvelle méthode pour obtenir le dernier ID reçu
-  getLatestReclamationId() {
-    return this.latestReclamationId;
+  getLastReclamationId(): string | null {
+    return this.lastReclamationId;
   }
 
-  
   async close() {
     await this.channel.close();
     await this.connection.close()
